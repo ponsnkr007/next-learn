6a8523e66afedbb684896054cbc18fc582a5f8b6
Sankar Chidambaram <sankarchidambaram@kpmg.com>
2025-08-05 14:01:34 +0530

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700']
+});
+export const inter = Inter({ subsets: ['latin'], weight: '500' });
